4572ea32a4ee12e458377460c21cf55fad228210
DataBigDawg <frasersusan@hotmail.com>
2016-01-21 22:57:56 -0500

    Programming Assignment 2 - R Programmg - Week 3 - Cache Inverted Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+##cachematrix.r   1/21/2016 Susan Fraser
+## makeCacheMatrix function
+##   -- create a special 'matrix' object that can cache its inverse.
+##
+## cacheSolve function
+##   --  computes the inverse of the special "matrix" returned by makeCacheMatrix
 
-## Write a short comment describing this function
+##  makeCacheMatrix contains 4 functions: set, get, setmatrix, getmatrix
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+      # set is a function that changes the vector stored in the main function (makeCacheMatrix)
+        set <- function(y) {
+            x <<- y
+            m <<- NULL
+        }
+      # get is a function that returns the vector x stored in the main function
+        get <- function() x
+   
+      # store the value of the input in a variable m into the main function makeCacheMtrix
+      # (setmean) and return it (getmean)
+        setmatrix <- function(x) m <<- solve(x)
+        getmatrix <- function() m
+        list(set = set, get = get,
+             setmatrix = setmatrix,
+             getmatrix = getmatrix)
+    }
 
-}
 
 
-## Write a short comment describing this function
+## cacheSolve will check for the cache of the matrix passed in
+## if found it will display 'getting cached data'
+## it will then save the matrix passed in
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x=matrix(), ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+    m <- x$getmatrix()
+    if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+    }
+    data <- x$get()
+    m <- data           
+    r <- x$setmatrix(m)
+    r 
+    }
+
